6dc418527ac9eda6a1c70232dadd1002c0f40dc6
Lutz <lutz.l.burning@gmail.com>
2017-06-28 16:42:29 +0800

    Change color of non master branch

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -10,7 +10,7 @@ function fish_prompt --description 'Write out the prompt'
     if __fish_vcs_prompt | grep -E '^ ?\(?master\)?$' > /dev/null
         set -g __fish_prompt_vcs (set_color red)
     else
-        set -g __fish_prompt_vcs "$__fish_prompt_normal"
+        set -g __fish_prompt_vcs "$__fish_prompt_cwd"
     end
 
     echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_vcs" (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
