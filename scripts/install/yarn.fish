a6f6e30617e33a4bcc9a6137b3be6e83c526c185
Lutz <lutz.l.burning@gmail.com>
2017-08-19 02:21:55 +0800

    move yarn global add out of existence check

--- scripts/install/yarn.fish
+++ scripts/install/yarn.fish
@@ -2,11 +2,10 @@
 
 if not brew ls --versions yarn > /dev/null
   brew install yarn
-
-  yarn global add eslint \
-     eslint-plugin-react \
-            babel-eslint \
-        create-react-app \
-             js-beautify
 end
 
+yarn global add eslint \
+   eslint-plugin-react \
+          babel-eslint \
+      create-react-app \
+           js-beautify
