76bf3bc795e64dfae8f9ec6565840ed3d9807c22
Lutz <lutz.l.burning@gmail.com>
2022-04-26 11:41:58 +0800

    cleanup

--- scripts/install_general.fish
+++ scripts/install_general.fish
@@ -9,6 +9,9 @@ switch (uname)
     sudo apt install -y $bins
 end
 
+set -l node_bins neovim js-beautify
+npm i -g $node_bins
+
 # add plug.vim to manage plugins
 curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
