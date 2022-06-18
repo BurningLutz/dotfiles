9600305a8a49383f2917f13e140ddfcfbcbda7e2
Lutz <lutz.l.burning@gmail.com>
2022-06-18 21:53:11 +0800

    add "for windows" section

--- README.md
+++ README.md
@@ -4,8 +4,14 @@ My dot files.
 
 # Prerequisites
 
-Install [Nix](https://nixos.org/download.html#nix-install-windows).
+Install [Nix](https://nixos.org/download.html).
 
 # General Installation
 
 Simply run `setup.sh`. Don't worry, it's idempotent. :tada::tada::tada:
+
+# For Windows
+
+1. Download and install fonts inside the `windows/fonts` dir.
+2. Replace Windows Terminal config file with `windows/windows-terminal/config.json`.
+3. For neovim's clipboard to work, install `win32yank`.
--- links/.config/fish/config.fish
+++ links/.config/fish/config.fish
@@ -20,23 +20,4 @@ if status --is-interactive
 
   # .local/bin
   fish_add_path --path $HOME/.local/bin
-
-  # python
-  # pyenv and pyenv virtualenv
-  set -x PYENV_ROOT $HOME/.pyenv
-  set -x PATH $PYENV_ROOT/bin $PATH
-  if type -q pyenv
-    pyenv init --path | source
-    # add pyenv completions
-    pyenv init - | source
-  end
-  # poetry
-  set -gx PATH $HOME/.poetry/bin $PATH
-
-  # haskell
-  # ghcup-env
-  set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME
-  test -f $HOME/.ghcup/env ; and set -gx PATH $HOME/.ghcup/bin $PATH
-  # cabal bin
-  set -gx PATH $HOME/.cabal/bin $PATH
 end
