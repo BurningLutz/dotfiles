cf8e5e07ee0699e063ba43ca9120c72657b4079a
Lutz <lutz.l.burning@gmail.com>
2022-08-15 09:36:31 +0800

    Change locale to C.UTF-8.

--- links/.config/fish/config.fish
+++ links/.config/fish/config.fish
@@ -7,7 +7,7 @@ if status --is-login
   # set locale archive for nix
   set -x LOCALE_ARCHIVE /usr/lib/locale/locale-archive
   # set lang
-  set -x LANG en_US.UTF-8
+  set -x LANG C.UTF-8
   # set true color
   set -x COLORTERM truecolor
   # cabal config location
