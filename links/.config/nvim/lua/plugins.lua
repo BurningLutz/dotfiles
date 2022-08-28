0f3e65950d7e0bb536d51ec92f4b0f629e8fd3ef
Lutz <lutz.l.burning@gmail.com>
2022-08-28 12:41:19 +0800

    Update plugins.lua.

--- links/.config/nvim/lua/plugins.lua
+++ links/.config/nvim/lua/plugins.lua
@@ -1,47 +1,42 @@
-return require("packer").startup(function (use)
-  -- packer.nvim itself
+require "packer".startup(function (use)
+  -- # packer.nvim itself.
   use "wbthomason/packer.nvim"
 
-  -- basis >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-
-  -- Useful toolbar
+  -- toolbar.
   use "liuchengxu/eleline.vim"
-  -- theme
+  -- theme.
   use "w0ng/vim-hybrid"
-  -- Dir tree, bookmarks and more
+  -- dir tree, bookmarks and more.
   use "scrooloose/nerdtree"
-  -- Show file change inline
+  -- show file change inline.
   use "airblade/vim-gitgutter"
-  -- Sub-word movements
+  -- sub-word movements.
   use "bkad/CamelCaseMotion"
   -- Surround.vim is all about "surroundings": parentheses, brackets, quotes,
   -- XML tags, and more. The plugin provides mappings to easily delete,
   -- change and add such surroundings in pairs.
   use "tpope/vim-surround"
-  -- git wrapper
+  -- git wrapper.
   use "tpope/vim-fugitive"
-  -- 🔗 The fancy start screen for Vim.
+  -- 🔗 the fancy start screen for Vim.
   use "mhinz/vim-startify"
-  -- Align something
+  -- align something.
   use "vim-scripts/Align"
-  -- Search and replace through the whole project
+  -- search and replace through the whole project.
   use "dyng/ctrlsf.vim"
-  -- auto-complete
+  -- auto completion.
   use { "neoclide/coc.nvim"
       , tag = "v0.0.81"
       }
-  -- repeat plugin map
+  -- repeat plugin map.
   use "tpope/vim-repeat"
-  -- comment stuffs easily
+  -- comment stuffs easily.
   use "tpope/vim-commentary"
-  -- auto close pairs
+  -- auto close pairs.
   use "BurningLutz/vim-autoclose"
-  -- block-wise alignment
+  -- block-wise alignment.
   use "BurningLutz/blockalign.nvim"
-  -- basis <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
-  -- web related >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-  -- markdown preview
+  -- markdown preview.
   use { "iamcco/markdown-preview.nvim"
       , run   = "cd app && npm install"
       , ft    = { "markdown" }
@@ -49,21 +44,19 @@ return require("packer").startup(function (use)
                   vim.g.mkdp_filetypes = { "markdown" }
                 end
       }
-  -- markdown generate toc
+  -- markdown generate toc.
   use "mzlogin/vim-markdown-toc"
-  -- table mode
+  -- markdown table mode.
   use "dhruvasagar/vim-table-mode"
-  -- web related <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  -- editorconfig
+  -- editorconfig.
   use "editorconfig/editorconfig-vim"
-
-  -- db
+  -- db.
   use "tpope/vim-dadbod"
   use "kristijanhusak/vim-dadbod-ui"
-
+  -- treesitter, the syntax parser providing highlighting and textobjects.
   use { "nvim-treesitter/nvim-treesitter"
       , run = function ()
-                require"nvim-treesitter.install".update { with_sync = true }
+                require "nvim-treesitter.install".update { with_sync = true }
               end
       }
   use "nvim-treesitter/nvim-treesitter-textobjects"
