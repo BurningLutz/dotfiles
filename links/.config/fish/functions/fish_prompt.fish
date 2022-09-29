5dc12d5e677068e11324fa1d65130a5b39999153
Lutz <lutz.l.burning@gmail.com>
2022-09-29 15:28:59 +0800

    Update fish prompt.

--- links/.config/fish/functions/fish_prompt.fish
+++ links/.config/fish/functions/fish_prompt.fish
@@ -1,22 +1,26 @@
 function fish_prompt --description 'Write out the prompt'
-    if not set -q __fish_prompt_normal
-        set -g __fish_prompt_normal (set_color normal)
-    end
+  if not set -q __fish_prompt_normal
+    set -g __fish_prompt_normal (set_color normal)
+  end
 
-    if not set -q __fish_prompt_cwd
-        set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-    end
+  if not set -q __fish_prompt_cwd
+    set -g __fish_prompt_cwd (set_color $fish_color_cwd)
+  end
 
-    if __fish_vcs_prompt | grep -E '^ ?\(?master\)?$' > /dev/null
-        set -g __fish_prompt_vcs (set_color red)
-    else
-        set -g __fish_prompt_vcs "$__fish_prompt_cwd"
-    end
+  if not set -q __fish_prompt_date
+    set -g __fish_prompt_date (set_color brblack)
+  end
 
-    echo -n -s "$USER" @ (prompt_hostname) ' ' "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_vcs" (__fish_vcs_prompt) "$__fish_prompt_normal" '> '
+  if __fish_vcs_prompt | grep -E '^ ?\(?master\)?$' > /dev/null
+    set -g __fish_prompt_vcs (set_color red)
+  else
+    set -g __fish_prompt_vcs "$__fish_prompt_cwd"
+  end
 
-    # tell WSL the current CWD
-    if type -q wslpath
-      printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"
-    end
+  echo -n -s $__fish_prompt_date(date +%T)\ $__fish_prompt_normal$USER@(prompt_hostname)\ $__fish_prompt_cwd(prompt_pwd) $__fish_prompt_vcs(__fish_vcs_prompt) $__fish_prompt_normal "> "
+
+  # tell WSL the current CWD
+  if type -q wslpath
+    printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"
+  end
 end
