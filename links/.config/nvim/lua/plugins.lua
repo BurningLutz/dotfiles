facb01a4a463cca6b71c6ba787206ae07f1677d5
Lutz <lutz.l.burning@gmail.com>
2023-02-21 10:35:15 +0800

    lua-ls: don't check third party libraries.

--- links/.config/nvim/lua/plugins.lua
+++ links/.config/nvim/lua/plugins.lua
@@ -129,7 +129,9 @@ require "packer".startup(function (use)
                    { capabilities = caps
                    , settings     = { Lua = { runtime     = { version = "LuaJIT" }
                                             , diagnostics = { globals = { "vim" } }
-                                            , workspace   = { library = vim.api.nvim_get_runtime_file("", true) }
+                                            , workspace   = { library         = vim.api.nvim_get_runtime_file("", true)
+                                                            , checkThirdParty = false
+                                                            }
                                             , telemetry   = { enable = false }
                                             }
                                     }
