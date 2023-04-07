e857c7c1d7636b7b7c834660b783845617121efa
Lutz <lutz.l.burning@gmail.com>
2023-04-07 23:00:20 +0800

    feat: add nix integrations to fish.

--- homefiles/.config/fish/config.fish
+++ homefiles/.config/fish/config.fish
@@ -1,8 +1,12 @@
 if status --is-login
+  # integrate nix
+  . ~/.nix-profile/etc/profile.d/nix.fish
+  set -x fish_complete_path ~/.nix-profile/share/fish/vendor_completions.d $fish_complete_path
+  set -x fish_function_path ~/.nix-profile/share/fish/vendor_functions.d $fish_function_path
   # .local/bin
-  fish_add_path --path $HOME/.local/bin
+  fish_add_path --path ~/.local/bin
   # pyenv
-  fish_add_path --path $HOME/.pyenv/bin
+  fish_add_path --path ~/.pyenv/bin
   # set lang
   set -x LANG en_US.UTF-8
   # my default editor is neovim
@@ -14,7 +18,7 @@ if status --is-login
   # set locale archive for nix
   set -x LOCALE_ARCHIVE /usr/lib/locale/locale-archive
   # cabal config location
-  set -x CABAL_CONFIG $HOME/.config/cabal/config
+  set -x CABAL_CONFIG ~/.config/cabal/config
   # ghcup use XDG
   set -x GHCUP_USE_XDG_DIRS 1
 end
