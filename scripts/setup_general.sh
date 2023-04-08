9408daeaddfaa1462259f9e9dae08a535197c897
Lutz <lutz.l.burning@gmail.com>
2023-04-08 22:20:55 +0800

    fix: broken rm.

--- scripts/setup_general.sh
+++ scripts/setup_general.sh
@@ -1,6 +1,6 @@
 # neovim files and packer.nvim
 rm -rf ~/.local/share/nvim
-rm ~/.config/nvim/plugin/packer_compiled.lua
+rm -rf ~/.config/nvim/plugin/packer_compiled.lua
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 nvim -c "autocmd User PackerComplete quitall" -c PackerSync
