d36812bc8de9d5b1e17aa0e29992bce853a9be30
Lutz <lutz.l.burning@gmail.com>
2023-04-13 10:28:46 +0800

    fix: Currently, `setup_general.sh` will delete the whole share dir of nvim, which is too aggressive. Now it will delete only the packer dir.

--- scripts/setup_general.sh
+++ scripts/setup_general.sh
@@ -1,5 +1,5 @@
 # neovim files and packer.nvim
-rm -rf ~/.local/share/nvim
+rm -rf ~/.local/share/nvim/site/pack/packer
 rm -rf ~/.config/nvim/plugin/packer_compiled.lua
 
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
