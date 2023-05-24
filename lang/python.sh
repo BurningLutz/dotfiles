1c94a221bf8873c3abd6db1e33b678ea91ab1dda
Lutz <lutz.l.burning@gmail.com>
2023-05-24 14:03:36 +0800

    feat: remove gtk related stuffs of python script.

--- lang/python.sh
+++ lang/python.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# common dependencies
+sudo apt install -y python3-venv           \
+
+# dependencies required by pyenv
 sudo apt install -y curl                   \
-                    gir1.2-gtk-3.0         \
                     libbz2-dev             \
-                    libcairo2-dev          \
                     libffi-dev             \
-                    libgirepository1.0-dev \
                     liblzma-dev            \
                     libncursesw5-dev       \
                     libreadline-dev        \
@@ -13,12 +14,7 @@ sudo apt install -y curl                   \
                     libssl-dev             \
                     libxml2-dev            \
                     libxmlsec1-dev         \
-                    llvm                   \
-                    pkg-config             \
-                    python3-dev            \
-                    python3-venv           \
                     tk-dev                 \
-                    wget                   \
                     xz-utils               \
                     zlib1g-dev             \
 
