e53e180aa395d7f7426a90d09936da505858fa32
Lutz <lutz.l.burning@gmail.com>
2023-10-10 09:52:22 +0800

    feat: upgrade llvm & clang to version 15.

--- nix/home.nix
+++ nix/home.nix
@@ -30,8 +30,8 @@ in rec
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
-    clang-tools_12
-    clang_12
+    clang-tools_15
+    clang_15
     clickhouse
     fd
     git
@@ -41,7 +41,7 @@ in rec
     gradle
     htop
     jsbeautifier
-    llvm_12
+    llvm_15
     lua-language-server
     mongodb-tools
     nodePackages.typescript-language-server
