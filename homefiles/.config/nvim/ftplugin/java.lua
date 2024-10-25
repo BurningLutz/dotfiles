71ec98755bd4aa595bd3ae94bcbe7be64963e3c4
Lutz <lutz.l.burning@gmail.com>
2024-10-25 15:05:35 +0800

    chore: allow only maven project for now.

--- homefiles/.config/nvim/ftplugin/java.lua
+++ homefiles/.config/nvim/ftplugin/java.lua
@@ -1,6 +1,6 @@
 local config =
 { cmd = { "jdtls" }
-, root_dir = vim.fs.root(0, {".git", "mvnw", "gradlew"})
+, root_dir = vim.fs.root(0, {".git", "pom.xml"})
 , capabilities = vim.tbl_deep_extend(
     "force"
   , vim.lsp.protocol.make_client_capabilities()
