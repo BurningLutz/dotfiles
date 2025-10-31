f81020a752ec19631036692f1dc950a6e60ef56c
flo <ameven_yang@163.com>
2025-10-31 14:32:44 +0800

    feat: prohibit modification when dap is running, and more suitable shortcut keys are set.

--- homefiles/.config/nvim/lua/dapmap.lua
+++ homefiles/.config/nvim/lua/dapmap.lua
@@ -1,41 +1,50 @@
 local dapmap = {}
 local nmaps = {}
-local mapopt = {}
 
-local function nmap(lhs, rhs)
+local function nmap(lhs, rhs, desc)
   nmaps[lhs] = vim.fn.maparg(lhs, "n", false, true)
-  vim.keymap.set("n", lhs, rhs, mapopt)
+  vim.keymap.set("n", lhs, rhs, { desc = desc })
 end
 local function resmap()
   for key, value in pairs(nmaps) do
     if value and not vim.tbl_isempty(value) then
-      vim.keymap.set("n", key, value.rhs, mapopt)
+      vim.keymap.set("n", key, value.rhs, value.opt)
     else
-      vim.keymap.del("n", key, mapopt)
+      vim.keymap.del("n", key)
     end
   end
   nmaps = {}
 end
 
-local map =
-{ continue   = { key = "<leader>dc", func = function () require "dap".continue() end}
-, pause      = { key = "<leader>dp", func = function () require "dap".pause() end}
-, run_last   = { key = nil, func = function () require "dap".run_last() end}
-, step_back  = { key = "<Up>", func = function () require "dap".step_back() end}
-, step_into  = { key = "<Right>", func = function () require "dap".step_into() end}
-, step_out   = { key = "<Left>", func = function () require "dap".step_out() end}
-, step_over  = { key = "<Down>", func = function () require "dap".step_over() end}
-, terminate  = { key = "<leader>dx", func = function () require "dap".terminate() end}
+local nkeymap =
+{ continue          = { "dd", "c"}
+, pause             = "p"
+, toggle_breakpoint = { "db", "a" }
+, clear_breakpoints = "cb"
+, step_back         = "u"
+, step_into         = "i"
+, step_out          = "o"
+, step_over         = "s"
+, terminate         = "x"
 }
 function dapmap.setup()
-  for _, value in pairs(map) do
-    if value and not vim.tbl_isempty(value) then
-      if value.key then
-        nmap(value.key, value.func)
+  local dap = require "dap"
+  vim.bo.modifiable = false
+
+  for key, value in pairs(nkeymap) do
+    if value then
+      local smap = function (lhs) nmap(lhs, function () dap[key]() end, key) end
+      if type(value) == "table" then
+        for _, i in ipairs(value) do
+          smap(i)
+        end
+      else
+        smap(value)
       end
     end
   end
   return function ()
+    vim.bo.modifiable = true
     resmap()
   end
 end
