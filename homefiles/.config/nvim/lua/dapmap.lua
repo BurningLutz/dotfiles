9793d6550a8fead0cb8dd0dfb91392d641bc1bfc
flo <ameven_yang@163.com>
2025-12-22 17:28:03 +0800

    feat: fix nvim-dap buffer readonly state management with filetype fil… (#15)
    * feat: fix nvim-dap buffer readonly state management with filetype filtering
    
    * feat: clean dict

--- homefiles/.config/nvim/lua/dapmap.lua
+++ homefiles/.config/nvim/lua/dapmap.lua
@@ -1,8 +1,6 @@
 local M = {}
 
 local function enable(keymap)
-  vim.bo.modifiable = false
-
   local dap = require "dap"
   local original_nmaps = {}
   for name, maps in pairs(keymap) do
@@ -16,8 +14,6 @@ local function enable(keymap)
 end
 
 local function disable(original_nmaps)
-  vim.bo.modifiable = true
-
   for key, value in pairs(original_nmaps) do
     if value and not vim.tbl_isempty(value) then
       vim.keymap.set("n", key, value.rhs, value.opt)
@@ -27,25 +23,28 @@ local function disable(original_nmaps)
   end
 end
 
-function M.setup(keymap)
-  local winid          = vim.api.nvim_get_current_win()
+function M.setup(keymap, filetype)
+  local tracked_bufs = {}
+
+  local function set_readonly(bufnr)
+    if filetype and vim.bo[bufnr].filetype ~= filetype then
+      return
+    end
+
+    if not tracked_bufs[bufnr] then
+      tracked_bufs[bufnr] = vim.bo[bufnr].modifiable
+    end
+    vim.bo[bufnr].modifiable = false
+  end
+
+  set_readonly(vim.api.nvim_get_current_buf())
   local original_nmaps = enable(keymap)
+
   local autocmds       =
   { vim.api.nvim_create_autocmd(
-      "WinEnter"
+      "BufEnter"
     , { callback = function ()
-          if winid == vim.api.nvim_get_current_win() then
-            original_nmaps = enable(keymap)
-          end
-        end
-      }
-    )
-  , vim.api.nvim_create_autocmd(
-      "WinLeave"
-    , { callback = function ()
-          if winid == vim.api.nvim_get_current_win() then
-            disable(original_nmaps)
-          end
+          set_readonly(vim.api.nvim_get_current_buf())
         end
       }
     )
@@ -55,7 +54,14 @@ function M.setup(keymap)
     for _, id in ipairs(autocmds) do
       vim.api.nvim_del_autocmd(id)
     end
+    for bufnr, original_modifiable in pairs(tracked_bufs) do
+      if vim.api.nvim_buf_is_valid(bufnr) then
+        vim.bo[bufnr].modifiable = original_modifiable
+      end
+    end
     disable(original_nmaps)
+    original_nmaps = {}
+    tracked_bufs   = {}
   end
 end
 
